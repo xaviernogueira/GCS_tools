a7324c61b720a5d563c66cb987387a9afbdfe9c5
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2020-05-11 17:25:58 -0700

    removed extraneous code block

--- GCS_statistical_analysis_XRN.py
+++ GCS_statistical_analysis_XRN.py
@@ -50,9 +50,10 @@ def analysis_setup(table_directory):
     print("Max stage is %sft" % max_stage)
     return [stages_dict,stage_stats_xl_dict,max_stage,stat_table_location]
 
-def stage_level_descriptive_stats(stages_dict,stage_stats_xl_dict,max_stage):
+def stage_level_descriptive_stats(stages_dict,stage_stats_xl_dict,max_stage,box_and_whisker=False):
     '''This function takes the stages_dict of pandas dataframes, and the stage_stats_xl dictionary, as well as calculated max stage as arguments.
-    It fills out the xlsx file for each respective stage height with mean, std, max, min, and median values for the stage as a hole and each landform'''
+    It fills out the xlsx file for each respective stage height with mean, std, max, min, and median values for the stage as a hole and each landform
+    If the box_and_whisker parameter is set to True (not default), then box and whisker plots comparing the W, Z, and C(W,Z)'''
     for stage in range(1,max_stage+1):
         print("Writing descriptive stats for stage %sft" % stage)
         stage_df = stages_dict['Stage_%sft' % stage]
@@ -73,6 +74,13 @@ def stage_level_descriptive_stats(stages_dict,stage_stats_xl_dict,max_stage):
 
         wb = xl.load_workbook(stage_stat_xl)
         ws = wb.active
+        ws.title = 'Descriptive stats'
+
+        ws.cell(row=2, column=1).value = 'MEAN' #Setting up titles on xl
+        ws.cell(row=3, column=1).value = 'STD'
+        ws.cell(row=4, column=1).value = 'MAX'
+        ws.cell(row=5, column=1).value = 'MIN'
+        ws.cell(row=6, column=1).value = 'MEDIAN'
 
         for field in list_of_fields: #add values in each field column
             field_index = int(list_of_fields.index(field))
@@ -82,26 +90,47 @@ def stage_level_descriptive_stats(stages_dict,stage_stats_xl_dict,max_stage):
             ws.cell(row=4, column=(1 + field_index)).value = high[field_index]
             ws.cell(row=5, column=(1 + field_index)).value = low[field_index]
             ws.cell(row=6, column=(1 + field_index)).value = medians[field_index]
-            ws.cell(row=2, column=1).value = 'MEAN'
-            ws.cell(row=3, column=1).value = 'STD'
-            ws.cell(row=4, column=1).value = 'MAX'
-            ws.cell(row=5, column=1).value = 'MIN'
-            ws.cell(row=6, column=1).value = 'MEDIAN'
 
-        list_of_codes = [-2,-1,0,1,2] #Code: -2 for oversized, -1 for constricted pool, 0 for normal channel, 1 for wide riffle, and 2 for nozzle
-        ws['F2'].value = 'Landform Code*:'
-        ws['F2'].value = '*Code: -2 for oversized, -1 for constricted pool, 0 for normal channel, 1 for wide riffle, and 2 for nozzle'
-        for code in list_of_codes:
+        list_of_codes = [-2,-1,0,1,2]
+        ws['F1'].value = '*Code: -2 for oversized, -1 for constricted pool, 0 for normal channel, 1 for wide riffle, and 2 for nozzle'
+
+        w_codes_list = [] #initiate lists of arrays to store data for multiple box plot
+        z_codes_list = []
+        cwz_codes_list = []
+        list_of_field_lists = [w_codes_list,z_codes_list,cwz_codes_list]
+        box_plot_dict  = dict(zip(list_of_fields,list_of_field_lists)) #This has W,Z, and W_s_Z_s as keys referncing lists that will store the data for each subplot
+
+        row_num = 2
+        for code in list_of_codes: #Calculating same descriptive stats for each landform, each table is sapced 7 cells apart
             code_df = stage_df.loc[stage_df['code'] == code, ['dist_down', 'W_s', 'Z_s', 'W_s_Z_s']]
+            ws.cell(row=row_num, column=7).value = ("Landform Code %s" % code) #Preparing the table
+            ws.cell(row=row_num + 1, column=7).value = 'MEAN'
+            ws.cell(row=row_num + 2, column=7).value = 'STD'
+            ws.cell(row=row_num + 3, column=7).value = 'MAX'
+            ws.cell(row=row_num + 4, column=7).value = 'MIN'
+            ws.cell(row=row_num + 5, column=7).value = 'MEDIAN'
+
             for field in list_of_fields:
-            ws.cell().value = (np.mean(stage_df.loc[:, field].to_numpy()))
-            ws.cell().value = (np.std(stage_df.loc[:, field].to_numpy()))
-            ws.cell().value = (np.max(stage_df.loc[:, field].to_numpy()))
-            ws.cell().value = (np.min(stage_df.loc[:, field].to_numpy()))
-            ws.cell().value = (np.median(stage_df.loc[:, field].to_numpy()))
+                field_index = int(list_of_fields.index(field))
+                ws.cell(row=row_num,column=(8+field_index)).value = str(field)
+                ws.cell(row=row_num+1,column=(8+field_index)).value = (np.mean(code_df.loc[:, field].to_numpy()))
+                ws.cell(row=row_num+2,column=(8+field_index)).value = (np.std(code_df.loc[:, field].to_numpy()))
+                ws.cell(row=row_num+3,column=(8+field_index)).value = (np.max(code_df.loc[:, field].to_numpy()))
+                ws.cell(row=row_num+4,column=(8+field_index)).value = (np.min(code_df.loc[:, field].to_numpy()))
+                ws.cell(row=row_num+5,column=(8+field_index)).value = (np.median(code_df.loc[:, field].to_numpy()))
+
+                if box_and_whisker == True:
+                    box_plot_dict[field].append(code_df.loc[:, field].to_numpy())
+            row_num += 7
 
         print("Stage %sft descriptive stats completed..." % stage)
         wb.save(filename=stage_stat_xl)
+
+        if box_and_whisker==True:
+            for field in list_of_fields:
+                plt.boxplot(box_plot_dict[field], patch_artist=True, labels=['Oversized', 'Const. Pool', 'Normal', 'Wide riffle', 'Nozzle'])\
+                plt.show()
+
     print("All descriptive stats completed!")
 
 
