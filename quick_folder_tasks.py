7c9dde077db3159435bda7857700e9696d340aca
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2021-03-24 16:15:48 -0400

    File transfer function

--- quick_folder_tasks.py
+++ quick_folder_tasks.py
@@ -1,6 +1,7 @@
 import arcpy
 import pandas
 import os
+import shutil
 from os import listdir
 
 top_level_directory = r'Z:\users\xavierrn\SoCoast_Final_ResearchFiles'
@@ -9,6 +10,7 @@ in_folder_name = '\\LINEAR_DETREND\\landform_analysis'  #  folder to transfer fi
 
 csv = pandas.read_csv(top_level_directory + '\\classified_sampled_reaches.csv')
 comids = csv['comid'].to_list()
+classes = csv['manual_class'].to_list()
 comids = [int(i) for i in comids]
 
 sc_suffix = ['SCO2', 'SCO3', 'SCO4', 'SCO5', 'SC00_new_adds']
@@ -30,12 +32,13 @@ def dem_to_hillshade(class_folders=sc_folders, comids_list=comids):
                 print('Hillshade made for comid %s, located @ %s' % (comid, hillshade))
 
 
-def transfer_files(class_folders, comids_list, in_folder_name, files=[], top_out_folder=top_level_out_directory):
+def transfer_files(class_folders, comids_list, classes_list=classes, in_folder_suffix=in_folder_name, top_out_folder=top_level_out_directory):
     for folder in sc_folders:
         sub_folders = [f.path for f in os.scandir(folder) if f.is_dir()]
 
     for count, comid in enumerate(comids):
         comid_folder = folder + '\\COMID%s' % comid
+        sc = int(classes[count])
 
         if comid_folder in sub_folders:
             out_fig_folder = top_out_folder + '\\SC0%s\\figures' % sc
@@ -46,7 +49,24 @@ def transfer_files(class_folders, comids_list, in_folder_name, files=[], top_out
             if not os.path.exists(out_tables_folder):
                 os.makedirs(out_tables_folder)
 
-        in
+        in_folder = comid_folder + in_folder_suffix
+
+        for file in listdir(in_folder):
+            split = os.path.splitext(file)
+            if split[1] in ['.png', '.pdf', '.jpeg']:
+                shuntil.copy(in_folder + '\\%s' % file, out_fig_folder + '\\%s' % file)
+
+            elif split[1] in ['.csv', '.xlsx']:
+                shuntil.copy(in_folder + '\\%s' % file, out_fig_folder + '\\%s' % file)
+
+            print('%s moved to %s' % (file, out_fig_folder + '\\%s' % file))
+
+
+# Run functions
+transfer_files(class_folders=sc_folders, comids_list=comids, classes_list=classes, in_folder_suffix=in_folder_name, top_out_folder=top_level_out_directory)
+
+
+
 
 
 
